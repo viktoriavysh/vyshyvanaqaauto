c47fa5d10bba7f690eb8edf384412639dfbec683
Kivi <kivi@weareclario.com>
2023-07-05 14:44:40 +0300

    delete all files

--- tests/api/test_fixture.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.mark.check
-def test_change_name(user):
-    assert user.name == 'Sergii'
-
-@pytest.mark.check
-def test_change_second_name(user):
-    assert user.second_name == 'Butenko'
